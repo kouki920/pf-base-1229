4002842f83048c6f4e7ff15e4e13be7c111ae0aa
kouki920 <74360349+kouki920@users.noreply.github.com>
2021-01-06 15:16:50 +0900

    Update index.js

--- Js/index.js
+++ Js/index.js
@@ -1,20 +1,20 @@
 'use strict';
 
-
+//anime.min.js
   let bouncingBall = anime({
-    targets: ['.header_title','.user_button_list'],
+    targets: ['.header_title','.user_button_list','about_box','#about_title'],
     translateY: '3vh',
     duration: 800,
     direction: 'alternate',
     easing: 'easeInCubic',
     scaleX: {
       value: 1.1,
-      duration: 150,
+      duration: 500,
       delay: 400
     }
   });
 
-
+//pageのtopに戻るbutton
   $(function () {
     let pagetop = $('#page_top');
     pagetop.hide();
@@ -38,3 +38,19 @@
         return false;
       });
   });
+
+//pf_messageにアニメーション付与
+  var textWrapper = document.querySelector('#pf_message');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: false})
+  .add({
+    targets: '#pf_message .letter',
+    translateX: [40,0],
+    translateZ: 0,
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 1200,
+    delay: (el, i) => 500 + 30 * i
+  });
+
